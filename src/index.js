8c3d54c579d12b65798d651c34e3ae111aac851b
rsharafeyev <r.sharafeyev@gmail.com>
2021-03-03 09:22:08 +0600

    init: added min,max,avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || !Array.isArray(array) || array.length == 0){return 0;}
+  let min = array[0];
+  for (let item of array)
+  {
+    if (item < min){min = item}
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || !Array.isArray(array) || array.length == 0){return 0;}
+  let max = array[0];
+  for (let item of array)
+  {
+    if (item > max){max = item}
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || !Array.isArray(array) || array.length == 0){return 0;}
+  let sum = 0;  
+  for (let item of array){sum += item}
+  return sum/array.length;
 }
